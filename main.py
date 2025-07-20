1e23d741d6c94fe775a3edb057231a8a7ea5dc74
pmendenhall <wmmendenhall@yahoo.com>
2025-07-20 07:57:45 -0600

    added dynamic book link

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
 from stats import count_words
 from stats import count_chars
 from stats import format_chars
@@ -9,12 +15,12 @@ def get_book_text(filepath):
 
 #print the contents of the "get_book_text" file to the console
 def main():
-    book_contents = get_book_text("books/frankenstein.txt")
+    book_contents = get_book_text(sys.argv[1])
     word_count = count_words(book_contents)
     characters = count_chars(book_contents)
     char_list = format_chars(characters)
     print("============ BOOKBOT ============")
-    print(f"Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {sys.argv[1]}...")
     print("----------- Word Count ----------")
     print(f"Found {word_count} total words")
     print("--------- Character Count -------")
